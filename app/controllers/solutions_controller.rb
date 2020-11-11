ed72ada8712be40d73f04971b6b78b1d9baceda5
Clare Forrest <clare.a.forrest@gmail.com>
2020-11-11 14:40:33 +1100

    corrected referencing for nested routes on solutions pages

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,5 +1,8 @@
 class SolutionsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_params, only:[:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only:[:edit, :update, :destroy]
+
   def index
     @solutions = Solution.all
   end
@@ -9,22 +12,28 @@ class SolutionsController < ApplicationController
   end
 
   def create
-    @problem = Problem.find(params[:id])
-    @solution = @problem.solutions.create(solution_params)
+    @problem = Problem.find(params[:problem_id])
+    @solution = @problem.solutions.new(solution_params)
+    @solution.user = current_user
+    @solution.save
     redirect_to problem_solutions_path
-    # @solution = Problem.solution.create(solution_params)
   end
 
   def show
   end
 
   def edit
+    @problem_id = params[:problem_id]
   end
 
   def update
+    @solution.update(solution_params)
+    redirect_to problem_solutions_path
   end
 
   def destroy
+    @solution.destroy
+    redirect_to problem_solutions_path
   end
 
   private
@@ -34,7 +43,14 @@ class SolutionsController < ApplicationController
   end
 
   def solution_params
-    params.require(:solution).permit(:title, :body)#also includes :id
+    params.require(:solution).permit(:title, :body)#also includes PK :id and FK user_id and problem_id
+  end
+
+  def authorize_user!
+    unless @solution.user == current_user
+      redirect_to problem_solutions_path(params[:problem_id])
+      #put in an alert here
+    end
   end
 
 end
