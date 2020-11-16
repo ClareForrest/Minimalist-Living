664fdc4c3d426dc9ce2dc1165578ff41c456101f
Clare Forrest <clare.a.forrest@gmail.com>
2020-11-16 12:09:39 +1100

    set up validations and alerts

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,4 +2,8 @@ class Problem < ApplicationRecord
   belongs_to :user
   has_many :solutions, dependent: :destroy
   has_one_attached :image
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length: { maximum: 500 }
+  validates :category, presence: true
 end
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -3,4 +3,7 @@ class Solution < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_one_attached :document
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length: { maximum: 500 }
 end
