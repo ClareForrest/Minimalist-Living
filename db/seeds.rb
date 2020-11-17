9ac32bf403e514cfe26af7fc46d79fc3bcca251a
Clare Forrest <clare.a.forrest@gmail.com>
2020-11-17 14:47:02 +1100

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,20 @@
+user1 = User.create(email: 'clare@test.com', password: '123456')
+user2 = User.create(email: 'francie@test.com', password: '123456')
+
 10.times do
-  Problem.create(title: 'problem heading - k', body: 'kitchen category', category: 'kitchen', user_id: 1)
+  Problem.create(title: 'problem heading - k', body: 'kitchen category', category: 'kitchen', user: user1)
 end
 
 10.times do
-  Problem.create(title: 'problem heading - h', body: 'hallway category', category: 'hallway', user_id: 1)
+  Problem.create(title: 'problem heading - h', body: 'hallway category', category: 'hallway', user: user2)
 end
 
 10.times do
-  Problem.create(title: 'problem heading - b', body: 'bedroom category', category: 'bedroom', user_id: 1)
+  Problem.create(title: 'problem heading - b', body: 'bedroom category', category: 'bedroom', user: user1)
 end
 
 10.times do
-  Problem.create(title: 'problem heading - bth', body: 'bathroom category', category: 'bathroom', user_id: 1)
+  p = Problem.create(title: 'problem heading - bth', body: 'bathroom category', category: 'bathroom', user: user2)
+  p.solutions.create(title: 'solution heading - bth', body: 'solution bathroom', price: 2, user: user2)
+  p.solutions.create(title: 'solution heading - bth', body: 'solution bathroom', price: 2, user: user1)
 end
-
-# def create
-#   @problem = Problem.find(params[:problem_id])
-#   @solution = @problem.solutions.new(solution_params)
-  
-#   @solution.user = current_user
-#   @solution.save
-  
-#   p @solution.errors.full_messages
-#   redirect_to problem_solutions_path
-#   end
